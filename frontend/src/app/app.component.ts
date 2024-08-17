6d39049b7b3f19fafc6df5d0728bb3aede93b722
Yagnnoz <jensk37@gmail.com>
2024-08-18 00:36:03 +0200

    changed to standalone application, added material and dark theme

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
 })
 export class AppComponent {
-  title = 'frontend';
+    title = 'frontend';
 }
