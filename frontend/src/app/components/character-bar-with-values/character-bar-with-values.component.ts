9cca69962a8572cf58fb5b7d528a1475b6d5471b
Yagnnoz <jensk37@gmail.com>
2024-08-18 15:12:47 +0200

    added color options for progress bar

--- frontend/src/app/components/character-bar-with-values/character-bar-with-values.component.ts
+++ frontend/src/app/components/character-bar-with-values/character-bar-with-values.component.ts
@@ -1,11 +1,13 @@
 import { Component, computed, input } from '@angular/core';
 import { MatProgressBar } from "@angular/material/progress-bar";
+import { NgClass } from "@angular/common";
 
 @Component({
     selector: 'app-character-bar-with-values',
     standalone: true,
     imports: [
-        MatProgressBar
+        MatProgressBar,
+        NgClass
     ],
     templateUrl: './character-bar-with-values.component.html',
     styleUrl: './character-bar-with-values.component.scss'
@@ -18,5 +20,16 @@ export class CharacterBarWithValuesComponent {
 
     percentage = computed(() => {
         return this.currentValue() * 100 / this.maxValue();
+    });
+
+    percentageColor = computed(() => {
+        switch (this.name()) {
+            case 'HP':
+                return 'red';
+            case 'Stamina':
+                return 'green';
+            default:
+                return 'blue';
+        }
     });
 }
